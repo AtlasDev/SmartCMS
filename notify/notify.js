cd436f57483e014154b56b1e34e2b524e7bf60cc
AtlasDev <danysluyk@live.nl>
2014-09-11 22:11:03 +0200

    Notifications first try

---
+++ notify/notify.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("#notify").hide(1000);
+    });
+    $("#show").click(function(){
+        $("#notify").show(1000);
+    });
+});
