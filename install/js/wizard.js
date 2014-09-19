0c0aaaad4c410805d7cadc38c1b7dfb11f6cb4a7
AtlasDev <danysluyk@live.nl>
2014-09-19 11:09:48 +0200

    Make the wizard work

---
+++ install/js/wizard.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#wizard').bootstrapWizard({'tabClass': 'nav nav-tabs', 'debug': false,
+    onTabShow: function(tab, navigation, index) {
+        var $total = navigation.find('li').length;
+        var $current = index+1;
+
+        if($current >= $total) {
+            $('#wizard').find('.pager .next').hide();
+            $('#wizard').find('.pager .finish').show();
+            $('#wizard').find('.pager .finish').removeClass('disabled');
+        } else {
+            $('#wizard').find('.pager .next').show();
+            $('#wizard').find('.pager .finish').hide();
+        }
+    }});
+});
