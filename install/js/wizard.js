d6b4883cbc90c668fe1e083541286db3595edbba
AtlasDev <danysluyk@live.nl>
2014-09-23 19:29:23 +0200

    work futher on installation

--- install/js/wizard.js
+++ install/js/wizard.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    $('#wizard').bootstrapWizard({'tabClass': 'nav nav-tabs', 'debug': false,
-    onTabShow: function(tab, navigation, index) {
-        var $total = navigation.find('li').length;
-        var $current = index+1;
+    $('#wizard').bootstrapWizard({'tabClass': 'nav nav-tabs',
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
 
-        if($current >= $total) {
-            $('#wizard').find('.pager .next').hide();
-            $('#wizard').find('.pager .finish').show();
-            $('#wizard').find('.pager .finish').removeClass('disabled');
-        } else {
-            $('#wizard').find('.pager .next').show();
-            $('#wizard').find('.pager .finish').hide();
+            if($current >= $total) {
+                $('#wizard').find('.pager .next').hide();
+                $('#wizard').find('.pager .finish').show();
+                $('#wizard').find('.pager .finish').removeClass('disabled');
+            } else {
+                $('#wizard').find('.pager .next').show();
+                $('#wizard').find('.pager .finish').hide();
+            }
         }
-    }});
+    });
 });
