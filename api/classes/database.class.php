91643ec6ec1d0977527bbd749b8efefbcc0df495
AtlasDev <danysluyk@live.nl>
2014-09-25 12:11:51 +0200

    Switch to PDO SQL

--- api/classes/database.class.php
+++ api/classes/database.class.php
@@ -11,33 +11,29 @@ class DB extends FlatFile {
     private $_DBport;
 
     public function __construct() {
-        $config = new FlatFile("db");
-        $this->_DBusername = $config->getContent("username");
-        $this->_DBpassword = $config->getContent("password");
-        $this->_DBdatabase = $config->getContent("database");
-        $this->_DBprefix = $config->getContent("prefix");
-        $this->_DBhost = $config->getContent("host");
-        $this->_DBport = $config->getContent("port");
-        $this->_connection = mysqli_connect($this->_DBhost.$this->_port, $_this->DBusername, $this->_DBpassword, $this->_DBdatabase);
-        if (mysqli_connect_errno($this->_connection)) {
+        if(PDO::getAvailableDrivers()=<0){
             return false;
-        } else {
-            return true;
         }
+        $this->_connection = new PDO('mysql:host='.$this->_DBhost.':'.$this->_DBport.';dbname='.$this->_DBdatabase, $this->_DBusername, $this->_DBpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $pdo->exec ("QUERY WITH SYNTAX ERROR");
+        } catch (PDOException $e) {
+            $code = $e->getCode();
+            $file = "../config/error.log";
+            $current = file_get_contents($file);
+            $current .= "[Error] ".date("Y-m-d H:i:s")." Mysql returned an error: ".$code."\n";
+            file_put_contents($file, $current);
+            return false;
+        } 
     }
 
     public function runQuery($query, $returnRaw=false) {
-        $result = $this->_connection->query($query);
-        if($returnRaw == true) {
-            $return = $query;
-        } else {
-            $return = $result->fetch_assoc();
-        }
-        return $return;
+        
     }
 
     public function getConfig($key) {
-        $result = $this->runQuery("SELECT * FROM {table_prefix} where key = " . $key);
+        
     }
 
 }
