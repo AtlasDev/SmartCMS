06314a8abcba4f8f3af30a71d479c951e67d4c74
AtlasDev <danysluyk@live.nl>
2014-09-27 19:24:38 +0200

    Updated classes and main api

--- api/classes/classCombiner.php
+++ api/classes/classCombiner.php
@@ -4,5 +4,6 @@ include("flatfile.class.php");
 include("database.class.php");
 include("page.class.php");
 include("user.class.php");
+include("lang.class.php")
 
 ?>
--- api/index.php
+++ api/index.php
@@ -2,8 +2,37 @@
 
 include("classes/classCombiner.php");
 
-if(empty($_POST[''])) {
+/**
+This is the hub of the api, the routing and response are defined here.
 
+Response codes:
+
+Global:
+0 = success
+1 = No request type defined
+2 = CMS not installed
+3 = database connection failed
+
+Page.class.php:
+4 = 
+
+**/
+
+$response = array("code" => "", "content" => "", "" => "");
+
+if(empty($_POST['type'])) {
+    json_encode($response);
+} else {
+    if(file_exists("config/installed")) {
+        $conn = new DB();
+        if($conn = false) {
+            $response["code"] = 3;
+            return json_encode($response);
+        }
+    } else {
+        $response["code"] = 2;
+        return json_encode($response);
+    }
 }
 
 ?>
