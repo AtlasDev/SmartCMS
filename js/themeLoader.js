aee24dd8d47ed4e4aa25c160c0a435090331be2c
AtlasDev <danysluyk@live.nl>
2014-10-06 10:14:45 +0200

    finally got json working

--- js/themeLoader.js
+++ js/themeLoader.js
@@ -1,7 +1,5 @@
 url = $(location).attr('href').substring(0, $(location).attr('href').lastIndexOf("/")) + "/api/";
-$.post(url, { type: "theme" })
+$.post(url, { type: "theme", dataType: 'json' })
     .done(function(data) {
-        $("body").append("sda" + data);
-        data = JSON.parse(data);
-        console.log(data);
+        console.log(data[0].destination);
     });
